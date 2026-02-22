18ca2b688a34b19114bb4f960f5693c64393192e
Anas <anasrajput99112@gmail.com>
2026-02-22 15:46:29 +0500

    add service section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const humbergerMenu = document.getElementById("menuBtn");
+const navBar = document.getElementById("navbar");
+
+humbergerMenu.addEventListener("click", () => {
+    navBar.classList.toggle("left-[50%]");
+    console.log("hellow");
+    
+});
+
+console.log("helloe");

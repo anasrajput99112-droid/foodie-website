d6eaf0c2e3a2a0a60874d94c5e81d60cf2614c97
Anas <anasrajput99112@gmail.com>
2026-04-04 20:02:00 +0500

    add total fun

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@ const cartIcon = document.getElementById("cart-icon");
 const cartTab = document.getElementById("cart-tab");
 const closeBtn = document.getElementById("close-btn");
 const cartList = document.getElementById("cart-list");
-const AddCartitems = document.getElementById("cart-items");
+// const AddCartitems = document.getElementById("cart-items");
+const cardList = document.getElementById("card-list");
 
 cartIcon.addEventListener("click", () => {
   cartTab.classList.toggle("left-auto");
@@ -31,22 +32,21 @@ closeBtn.addEventListener("click", () => {
 });
 
 let productList = [];
+let cartProduct = [];
 
 const showCards = () => {
-  cartList.innerHTML = ""; // clear old entries before re-render
   productList.forEach((product) => {
     const orderCard = document.createElement("div");
     orderCard.classList.add("cart-list");
     orderCard.innerHTML = `
-     <div
-            class="flex flex-col justify-center items-center gap-5 bg-[#fff] border-2 border-amber-300  py-10 px-10 lg:py-5 lg:px-5 rounded-[1.5rem] shadow-[0px_8px_16px_rgba(0,0,0,0.1),inset_0px_1px_2px_rgba(255,255,255,0.8),inset_0px_-1px_2px_rgba(0,0,0,0.05)] ">
+           <div class="flex flex-col justify-center items-center gap-5 bg-[#fff] border-2 border-amber-300  py-10 px-10 lg:py-5 lg:px-5 rounded-[1.5rem] shadow-[0px_8px_16px_rgba(0,0,0,0.1),inset_0px_1px_2px_rgba(255,255,255,0.8),inset_0px_-1px_2px_rgba(0,0,0,0.05)] ">
             <div>
               <img class="w-45 lg:w-52" src="${product.image}" alt="${product.name}">
             </div>
             <div class="text-center">
               <h1 class="text-xl font-bold">${product.name}</h1>
 
-              <span class="text-[#F2bd12] font-bold">$${product.price}</span>
+              <span class="text-[#F2bd12] font-bold">${product.price}</span>
             </div>
             <div>
               <button class="btn card-btn">Add to cart</button>
@@ -55,16 +55,76 @@ const showCards = () => {
           </div>`;
 
     cartList.appendChild(orderCard);
-    const addToCartBtn = orderCard.querySelector(".card-btn");
-      addToCartBtn.addEventListener("click", (e) => {
-         e.preventDefault();
-         alert(`${product.name} added to cart!`);
-         addToCart()
-      });
 
+    const CartBtn = orderCard.querySelector(".card-btn");
+    CartBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      addToCart(product);
+    });
   });
 };
 
+const addToCart = (product) => {
+  const existingProduct = cartProduct.find((item) => item.id === product.id);
+  if (existingProduct) {
+    alert("Product already in cart");
+    return;
+  }
+
+  cartProduct.push(product);
+
+  let quantity = 1;
+  let Price = parseFloat(product.price.replace("$", ""));
+
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("items");
+  cartItem.innerHTML = `
+                 <div class=" items item-list flex items-center gap-5 mb-2.5 p-5">
+                 <div>
+                  <img class="card-image w-[5rem] h-[5rem] object-cover" src="${product.image}">
+                 </div>
+
+                 <div>
+                  <h4 class="text-[1.1rem] font-[400]">${product.name}</h4>
+                  <h4 class="item-total text-[1rem]">${product.price}</h4>
+                 </div>
+
+                 <div class="flex ">
+                  <a href="" class="minus bg-black text-white w-[1.7rem] aspect-[1] rounded-[100vw] text-center leading-[1.7rem]"><i class="ri-subtract-fill"></i></a>
+                  <h4 class="quantity-value text-[1rem] font-bold mx-[.3rem]">1</h4>
+                  <a href="" class="plus  bg-black text-white w-[1.7rem] aspect-[1] rounded-[100vw] text-center leading-[1.7rem]"><i class="ri-add-fill"></i></a>
+                 </div> 
+                 </div>`;
+  cardList.appendChild(cartItem);
+
+  const plusBtn = cartItem.querySelector(".plus");
+  const minusBtn = cartItem.querySelector(".minus");
+  const quantityValue = cartItem.querySelector(".quantity-value");
+  const itemTotal = cartItem.querySelector(".item-total");
+
+  plusBtn.addEventListener("click", (e) => {
+    quantity++;
+    quantityValue.textContent = quantity;
+    itemTotal.textContent = `$${(Price * quantity).toFixed(2)}`;
+    e.preventDefault();
+  });
+
+  minusBtn.addEventListener("click", (e) => {
+    if (quantity > 1) {
+      quantity--;
+      quantityValue.textContent = quantity;
+      itemTotal.textContent = `$${(Price * quantity).toFixed(2)}`;
+    } else {
+     setTimeout(() => {
+      cartItem.remove();
+     }, 300);
+     cartProduct = cartProduct.filter((item) => item.id !== product.id);
+     alert("Product removed from cart");
+    }
+
+    e.preventDefault();
+  });
+};
 
 const initApp = () => {
   fetch("products.json")
